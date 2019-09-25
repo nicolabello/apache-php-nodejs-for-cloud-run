780b60d09ed67506da1ac97562fab66031a5bf65
Nicola Bello <nicola.bello.3@gmail.com>
2019-09-25 18:30:11 +0100

    set fixed tag

--- docker-run.sh
+++ docker-run.sh
@@ -7,9 +7,9 @@ appFolder=${1:-"example-app"}
 
 scriptPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #executionPath=$(pwd)
-tag="$(basename "$scriptPath"):latest"
+tag="nicolabello/apache-php-nodejs-for-cloud-run:latest"
 
 # Add --rm to build or run to remove automatically on stop
 docker rm $(docker stop $(docker ps -a -q --filter "ancestor=$tag" --format="{{.ID}}")) || true
 docker build --rm -t "$tag" .
-docker run -d -p 90:8080/tcp -v "$scriptPath/$appFolder:/apps/$appFolder:ro" "$tag"
+docker run -d -p 90:8080/tcp -t "$tag"
